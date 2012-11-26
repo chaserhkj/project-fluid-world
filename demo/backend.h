a12466a12778ba33016fae8107e3ff0a1a93913b
Light <sgsdxzy@gmail.com>
2012-11-26 19:49:49 +0800

    add header files.

---
+++ demo/backend.h
@@ -0,0 +1,11 @@
+#ifndef _backend
+#define _backend
+extern const double step;
+extern const gravity;
+extern double theta;
+extern double velocity;
+extern double length;
+extern double time;
+extern void pendulum(void)
+#endif
+
