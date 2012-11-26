1c81a5a583a4a04d8bd80d0b7a2e9dc41830bfd8
Light <sgsdxzy@gmail.com>
2012-11-26 19:44:12 +0800

    implemented backend

--- demo/backend.cpp
+++ demo/backend.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include <cmath>
+
+const double step = 0.01;
+const gravity = 9.8;
+double theta;
+double velocity;
+double length;
+double time;
+
+void pendulum(void)
+{
+    double acceleration = -gravity*sin(theta);
+    theta += (v * step + acceleration * step * step/2)/length;
+    v += acceleration * step;
+    time += step;
+}
+
