91e95f93456f938f395ceb2604d2fa931a6ad252
Light <sgsdxzy@gmail.com>
2012-11-28 15:06:26 +0800

    Unite coding style

--- demo/backend.h
+++ demo/backend.h
@@ -2,22 +2,22 @@
 #define _backend
 class pendulum
 {
-    private:
-        double theta, velocity, length, step;
-    public:
-        pendulum(double t = 0, double v = 0, double l = 1, double s=0.1):\
-            theta(t), velocity(v), length(l), step(s){}
-        void setStatus(double t, double v){
-            theta = t;
-            velocity = v;
-        }
-        double getAngle() const{
-            return theta;
-        }
-        double getVelocity() const{
-            return velocity;
-        }
-        void run();
+private:
+    double theta, velocity, length, step;
+public:
+    pendulum(double t = 0, double v = 0, double l = 1, double s=0.1):\
+        theta(t), velocity(v), length(l), step(s){}
+    void setStatus(double t, double v){
+        theta = t;
+        velocity = v;
+    }
+    double getAngle() const{
+        return theta;
+    }
+    double getVelocity() const{
+        return velocity;
+    }
+    void run();
 };
 #endif
 
