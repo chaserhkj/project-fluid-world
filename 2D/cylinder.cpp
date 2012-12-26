a4d05780e6dffb41f15d1eeb4764be752c8ccd04
Light <sgsdxzy@gmail.com>
2012-12-26 09:30:16 +0800

    fix lame coeficient

--- 2D/cylinder.cpp
+++ 2D/cylinder.cpp
@@ -22,14 +22,17 @@ void cylinderNode::calculateXY()
 void cylinderNode::run(double psi1, double psi2, double psi3, double psi4)
 {
     //recursive calcultion of Psi
-   double g1 = 1, g2 = 1, g3 = 0, g4 = 0;
-   double b0, b1, b2, b3, b4;
-   b0 = 2 * g1 / (deltaxi * deltaxi) + 2 * g2 / (deltaeta * deltaeta);
-   b1 = g1 / (deltaxi * deltaxi) + g3 / (2 * deltaxi);
-   b2 = g1 / (deltaxi * deltaxi) - g3 / (2 * deltaxi);
-   b3 = g2 / (deltaeta * deltaeta) + g4 / (2 * deltaeta);
-   b4 = g2 / (deltaeta * deltaeta) - g4 / (2 * deltaeta);
-   double omega = 1.618; /* relaxation coefficient */
-   newpsi = (1 - omega) * psi + omega * (b1 * psi1 + b2 * psi2 + b3 * psi3 + b4 * psi4 + zeta) / b0;
-   //TODO: newzeta
+    double hxi, heta; /* lame coefficient */
+    hxi = sqrt(1 + (1 + x * x - y * y) / ((x * x + y * y) * (x * x + y * y))) / 2;
+    heta = hxi;
+    double g1 = 1 / (hxi * hxi), g2 = 1 / (heta * heta), g3 = 0, g4 = 0;
+    double b0, b1, b2, b3, b4;
+    b0 = 2 * g1 / (deltaxi * deltaxi) + 2 * g2 / (deltaeta * deltaeta);
+    b1 = g1 / (deltaxi * deltaxi) + g3 / (2 * deltaxi);
+    b2 = g1 / (deltaxi * deltaxi) - g3 / (2 * deltaxi);
+    b3 = g2 / (deltaeta * deltaeta) + g4 / (2 * deltaeta);
+    b4 = g2 / (deltaeta * deltaeta) - g4 / (2 * deltaeta);
+    double omega = 1.618; /* relaxation coefficient */
+    newpsi = (1 - omega) * psi + omega * (b1 * psi1 + b2 * psi2 + b3 * psi3 + b4 * psi4 + zeta) / b0;
+    //TODO: newzeta
 }
