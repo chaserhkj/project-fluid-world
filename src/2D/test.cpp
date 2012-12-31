ada464ba074d2afccb62edaaca0a3e74f45b0b10
Light <sgsdxzy@gmail.com>
2012-12-31 11:17:18 +0800

    wrapped lines

--- src/2D/test.cpp
+++ src/2D/test.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     int i;
-    cylinderProject* testproject; 
-    testproject = new cylinderProject(-1000,4000,1000,-1000,0.01,0.01,0.01,40,1.618);
+    cylinderProject *testproject;
+    testproject =
+	new cylinderProject(-1000, 4000, 1000, -1000, 0.01, 0.01, 0.01, 40,
+			    1.618);
     testproject->initialize();
-    for (i=0;i<1000;i++){
-        testproject->run();
-        cout << "One step:" << testproject->getT() << endl;
+    for (i = 0; i < 1000; i++) {
+	testproject->run();
+	cout << "One step:" << testproject->getT() << endl;
     }
     delete testproject;
     return 0;
